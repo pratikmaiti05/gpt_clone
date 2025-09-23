6a78cc28628cc7caf52a22c6f5c9b2f2e0d29359
pratikmaiti05 <maitipratik83@gmail.com>
2025-09-23 22:52:58 +0530

    Fix case-sensitive import for Register page

--- frontend/src/routes/Mainroutes.jsx
+++ frontend/src/routes/Mainroutes.jsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom"
 import Home from "../pages/Home"
 import Login from "../pages/Login"
-import Register from "../pages/register"
+import Register from "../pages/Register"
 const Mainroutes = () => {
   return (
     <Routes>
